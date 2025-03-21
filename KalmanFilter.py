fca17c417f1e47e5684226455380d1c6df48bc79
Julian Bolivar <jbolivarg@gmail.com>
2025-03-21 11:19:34 -0400

    Signal plot function and rms value calculation included to improve the fitness calculation and improve the algorithms convergence

--- KalmanFilter.py
+++ KalmanFilter.py
@@ -44,7 +44,7 @@ class KalmanFilter:
 
     def filter(self, measurements):
         """Apply Kalman filter to a sequence of measurements."""
-        return [self.filter_step(z) for z in measurements]
+        return np.array([self.filter_step(z) for z in measurements])
 
     def calculate_error(self, true_signal, filtered_signal):
         """Calculate error metrics between true and filtered signals."""
@@ -52,10 +52,61 @@ class KalmanFilter:
         rmse = np.sqrt(mse)  #Root Mean Square Error
         return mse, rmse
 
-    def plot_results(self, input_signal, true_signal, filtered_signal):
-        """Plot the original, noisy, and filtered signals."""
-        plt.plot(input_signal, label='Noisy Signal')
-        plt.plot(true_signal, label='Original Signal')
-        plt.plot(filtered_signal, label='Filtered Signal')
+    def calculate_rms_value(self, signal):
+        """Calculate the RMS value of a signal."""
+        return np.sqrt(np.mean(signal ** 2))
+
+    def calculate_rms_value_error(self, true_signal, filtered_signal):
+        """
+        Calculate the RMS value error between the true signal and the filtered signal.
+
+        Parameters:
+        - true_signal: The true signal without noise
+        - filtered_signal: The signal after applying the Kalman filter
+
+        Returns:
+        - rms_value_error: The relative RMS value error between the true and filtered signals
+        """
+        org_rms = np.sqrt(np.mean(true_signal ** 2))
+        filt_rms = np.sqrt(np.mean(filtered_signal ** 2))
+        rms_value_error = np.abs(org_rms - filt_rms)
+        return rms_value_error
+
+    def plot_results(self, noisy_signal, true_signal, filtered_signal, time, zoom_begin=0, zoom_end=1, title=''):
+        """
+        Plot the results of the Kalman filter.
+
+        Parameters:
+        - noisy_signal: The noisy input signal
+        - true_signal: The true signal without noise
+        - filtered_signal: The signal after applying the Kalman filter
+        - time: Array of time values corresponding to the signals
+        - zoom_begin: Fraction of the signal to start the zoomed plot (default is 0 for begginning)
+        - zoom_end: Fraction of the signal to end the zoomed plot (default is 1 for end)
+        - title: Title of the plot (default is an empty string)
+        """
+        if not (0 <= zoom_begin < zoom_end <= 1):
+            if zoom_begin < 0:
+                zoom_begin = 0
+            if zoom_end > 1:
+                zoom_end = 1
+            if zoom_begin >= zoom_end:
+                zoom_begin, zoom_end = zoom_end, zoom_begin
+        zoom_samples_begin = int(zoom_begin * len(time))
+        zoom_samples_end = int(zoom_end * len(time))
+        plt.figure(figsize=(12, 6))
+        plt.plot(time[zoom_samples_begin:zoom_samples_end], noisy_signal[zoom_samples_begin:zoom_samples_end], 'gray', alpha=0.5, label='Noisy Signal')
+        plt.plot(time[zoom_samples_begin:zoom_samples_end], true_signal[zoom_samples_begin:zoom_samples_end], 'b', linewidth=2.5, label='Original Signal')
+        plt.plot(time[zoom_samples_begin:zoom_samples_end], filtered_signal[zoom_samples_begin:zoom_samples_end], 'r',linewidth=2.5, label='Filtered Signal')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        if title:
+            plt.title(title)
+        else:
+            if zoom_begin == 0 and zoom_end == 1:
+                plt.title(f'Signal Filtered with Kalman Filter')
+            else:
+                plt.title(f'Signal Filtered with Kalman Filter (Zoomed)')
         plt.legend()
+        plt.grid(True)
         plt.show()
